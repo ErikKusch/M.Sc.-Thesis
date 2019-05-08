e24461ccb7906b954ba142d064da9d1b209a0191
Erik Kusch <erik@master-of.net>
2019-05-08 23:17:09 +0200

    Added GoT package

--- Y - Codes/S0a_Packages.R
+++ Y - Codes/S0a_Packages.R
@@ -3,6 +3,6 @@ install.load.package <- function(x) {
     install.packages(x, repos='http://cran.us.r-project.org')
   require(x, character.only = TRUE)
 }
-package_vec <- c("automap", "doParallel", "foreach", "ggplot2", "gimms", "gridExtra",
-                 "ndcf4", "pracma", "raster", "rgbif", "rgdal", "sp", "vegan", "xlsx")
+package_vec <- c("automap", "doParallel", "foreach", "gameofthrones", "ggplot2", "gimms", 
+                 "gridExtra", "ndcf4", "pracma", "raster", "rgbif", "rgdal", "sp", "vegan", "xlsx")
 sapply(package_vec, install.load.package)
